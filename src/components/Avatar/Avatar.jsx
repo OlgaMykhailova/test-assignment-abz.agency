92af8b3a25ce665783d47598e222a901337c89bc
OlgaMykhailova <rosebud_z@yahoo.com>
2023-04-07 19:43:16 +0300

    updated avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -9,6 +9,10 @@ export const Avatar = ({ photo }) => {
       height="70"
       className="avatar"
       loading="lazy"
+      onError={e =>
+        (e.currentTarget.src =
+          'https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg')
+      }
     />
   );
 };
