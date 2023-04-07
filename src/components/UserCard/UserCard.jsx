e312d3cef5988ce19f532e2a751d993cbf4a1d07
OlgaMykhailova <rosebud_z@yahoo.com>
2023-04-07 15:49:02 +0300

    updated tooltip

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,15 +1,23 @@
-import { useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Tooltip } from 'components/Tooltip/Tooltip';
 import './UserCard.scss';
 
 export const UserCard = ({ user }) => {
+  const [isNameOverflowHidden, setIsNameOverflowHidden] = useState(false);
+  const [isEmailOverflowHidden, setIsEmailOverflowHidden] = useState(false);
   const refName = useRef(null);
   const refEmail = useRef(null);
   const { photo, name, position, email, phone } = user;
-  const isNameOverflowHidden =
-    refName.current?.scrollWidth > refName.current?.offsetWidth;
-  const isEmailOverflowHidden =
-    refEmail.current?.scrollWidth > refEmail.current?.offsetWidth;
+
+  useEffect(() => {
+    if (refName.current?.scrollWidth > refName.current?.offsetWidth) {
+      setIsNameOverflowHidden(true);
+    }
+    if (refEmail.current?.scrollWidth > refEmail.current?.offsetWidth) {
+      setIsEmailOverflowHidden(true);
+    }
+  }, []);
+
   return (
     <li className="usercard">
       <img
